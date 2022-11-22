007d4d337f315c1a2ba37743cecba9b5333282d3
Yolknet <yolknet@users.noreply.github.com>
2022-11-22 15:01:16 +0100

    Update addcodetohead.php
    Added trim() function on line 44

--- addcodetohead.php
+++ addcodetohead.php
@@ -41,7 +41,7 @@ class plgSystemAddcodetohead extends CMSPLugin
 				new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 				j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 				'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-				})(window,document,'script','dataLayer','" . $containerId . "');</script>";
+				})(window,document,'script','dataLayer','" . trim($containerId) . "');</script>";
 				
 			return $tagManager;
 		}
